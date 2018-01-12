eb6a702067dcf491bd8e898c5c678f845a4390b7
zhangxiaokang <kangeneral@126.com>
2018-01-12 22:50:13 +0800

    Initial commit

---
+++ colas-sns/colas-sns-web/src/main/java/com/zhang/sns/SnsApplication.java
@@ -0,0 +1,15 @@
+package com.zhang.sns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zxk
+ * @date 2018-01-12 22:42:58
+ */
+@SpringBootApplication
+public class SnsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SnsApplication.class, args);
+    }
+}
