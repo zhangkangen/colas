217a59d0e94b1997c49bea4ab782a5c6228ab397
zhangxiaokang <zhangxiaokang@bainuo.com>
2018-01-13 21:56:07 +0800

    自动配置

--- colas-sns/colas-sns-web/src/main/java/com/zhang/sns/SnsApplication.java
+++ colas-sns/colas-sns-web/src/main/java/com/zhang/sns/SnsApplication.java
@@ -1,12 +1,14 @@
 package com.zhang.sns;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author zxk
  * @date 2018-01-12 22:42:58
  */
+@EnableAutoConfiguration
 @SpringBootApplication
 public class SnsApplication {
     public static void main(String[] args) {
