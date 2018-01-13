5126037b797c9b591a2a6a418e88315ce3043ec0
zhangxiaokang <kangeneral@126.com>
2018-01-14 00:24:26 +0800

    generatorConfig

--- colas-sns/colas-sns-web/src/main/java/com/zhang/sns/SnsApplication.java
+++ colas-sns/colas-sns-web/src/main/java/com/zhang/sns/SnsApplication.java
@@ -3,12 +3,14 @@ package com.zhang.sns;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author zxk
  * @date 2018-01-12 22:42:58
  */
-@EnableAutoConfiguration
+//开启定时任务
+//@EnableScheduling
 @SpringBootApplication
 public class SnsApplication {
     public static void main(String[] args) {
