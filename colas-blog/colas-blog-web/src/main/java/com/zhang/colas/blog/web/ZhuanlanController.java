1992cb9c41ab44d286aae6aa189f4e8246ce104a
zhangxiaokang <kangeneral@126.com>
2018-02-14 23:03:12 +0800

    专栏

--- colas-blog/colas-blog-web/src/main/java/com/zhang/colas/blog/web/ZhuanlanController.java
+++ colas-blog/colas-blog-web/src/main/java/com/zhang/colas/blog/web/ZhuanlanController.java
@@ -1,13 +1,15 @@
 package com.zhang.colas.blog.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("zhuanlan")
+@RequestMapping(value = {"zhuanlan"})
 public class ZhuanlanController {
 
-    public String index(){
-        return "zhuanlan";
+    @GetMapping({"index", ""})
+    public String index() {
+        return "zhuanlan/index";
     }
 }
